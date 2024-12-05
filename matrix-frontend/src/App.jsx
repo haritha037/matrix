bfff3261dcc8a38d719075cd134c1ea9bb2e40b3
haritha037 <e20037@eng.pdn.ac.lk>
2024-12-05 19:02:48 +0530

    created the dummy pages and defined the routing

--- matrix-frontend/src/App.jsx
+++ matrix-frontend/src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AppLayout from "./UI/AppLayout";
+import Question from "./pages/Question";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
-  return <div>hii</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<Navigate replace to="/question" />} />
+          <Route path="question" element={<Question />} />
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
